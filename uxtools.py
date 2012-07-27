e3723f02ff8c70a986cf78bbbab86fc3028961d4
Juan Carlos del Valle <jc.ekinox@gmail.com>
2012-07-27 00:16:42 -0500

    all kitukids issues test

--- uxtools.py
+++ uxtools.py
@@ -1,5 +1,7 @@
 import sublime, sublime_plugin, re, subprocess, json
 
+issues = {}
+
 def get_window(self):
     return self.view.window() or sublime.active_window()
 
@@ -26,6 +28,120 @@ def _output_to_view(self, output_file, output, clear=False,
         output_file.erase(edit, region)
     output_file.insert(edit, 0, output)
     output_file.end_edit(edit)
+
+class uxtool_list_all_issues(sublime_plugin.TextCommand):
+    def run(self,edit):
+        data = {
+            "state" : "closed"
+        }
+
+        self.regions = {}
+
+        self.result_view = get_window(self).new_file()
+        self.result_view.set_scratch(True)
+        self.edit = self.result_view.begin_edit()
+        
+        self.print_c("Issues:")
+        self.print_c("")
+        self.print_c("--- OPENED ---")
+
+        for issue in self.get_issues("open"):
+            self.insert_issue(issue)
+
+        self.print_c("")
+        self.print_c("--- CLOSED ---")
+
+        for issue in self.get_issues("closed"):
+            self.insert_issue(issue);
+
+        self.result_view.end_edit(self.edit)
+
+        self.result_view.add_regions('results', self.regions.keys(), '')
+        self.result_view.settings().set('line_padding_bottom', 2)
+        self.result_view.settings().set('line_padding_top', 2)
+        self.result_view.settings().set('word_wrap', False)
+        self.result_view.settings().set('command_mode', True)
+
+    def insert_issue(self, issue):
+        global issues
+        issues[issue['number']] = issue
+
+        insert_point = self.result_view.size()
+        self.print_c('#' + str(issue['number']) + ' - ' + issue['title'], )
+        rgn = sublime.Region(insert_point, self.result_view.size())
+        self.regions[rgn] = issue
+
+    def print_c(self, str):
+        self.result_view.insert(self.edit, self.result_view.size(), str + "\n")
+
+    def get_issues(self, status):
+        token = sublime.load_settings("Preferences.sublime-settings").get("git_token")
+        issues, load_info = subprocess.Popen(["curl",
+            "-H","Authorization: token %s" % token,
+            "https://api.github.com/repos/ergoux/kitukids/issues?state=%s" % status],
+            stdout=subprocess.PIPE,stderr=subprocess.PIPE).communicate()
+        return json.loads(issues)
+
+class navigate_results(sublime_plugin.TextCommand):
+    DIRECTION = {'forward': 1, 'backward': -1}
+    STARTING_POINT = {'forward': -1, 'backward': 0}
+
+    def __init__(self, view):
+        super(navigate_results, self).__init__(view)
+
+    def run(self, edit, direction):
+        view = self.view
+        settings = view.settings()
+        results = self.view.get_regions('results')
+        if not results:
+            sublime.status_message('No results to navigate')
+            return
+
+        ##NOTE: numbers stored in settings are coerced to floats or longs
+        selection = int(settings.get('selected_result', self.STARTING_POINT[direction]))
+        selection = selection + self.DIRECTION[direction]
+        try:
+            target = results[selection]
+        except IndexError:
+            target = results[0]
+            selection = 0
+
+        settings.set('selected_result', selection)
+        ## Create a new region for highlighting
+        target = target.cover(target)
+        view.add_regions('selection', [target], 'selected', 'dot')
+        view.show(target)
+
+class clear_selection(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.view.erase_regions('selection')
+        self.view.settings().erase('selected_result')
+
+class goto_issue(sublime_plugin.TextCommand):
+    def __init__(self, *args):
+        super(goto_issue, self).__init__(*args)
+
+    def run(self, edit):
+        global issues
+
+        iss = issues
+        ## Get the idx of selected result region
+        selection = int(self.view.settings().get('selected_result', -1))
+        ## Get the region
+        selected_region = self.view.get_regions('results')[selection]
+        
+        data = self.view.substr(selected_region)
+        issue_num = re.search(r'#(.+) -',data).group(1)
+        selected_issue = iss[int(issue_num)]
+        print selected_issue
+        scratch(self,
+            "+ %s\n--- %s\n\n%s\n\n----------\n@@ %s @@" % (
+                selected_issue['title'],
+                "ergoux/kitukids -> issue #" + str(selected_issue['number']),
+                selected_issue['body'],
+                selected_issue['updated_at']
+                ),selected_issue['title'])
+        #sublime.active_window().run_command('open_url', {"url": selected_issue['html_url']})
 
 class uxtool_list_issues(sublime_plugin.TextCommand):
     def run(self,edit):
